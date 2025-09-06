5b18c584cce6c1a39fbed37f8ebb45181d8ac7f5
Tuka tuka <ryuseifgo@gmail.com>
2025-09-06 19:02:08 +0900

    必要なライブラリの意のインポート

--- task-management/src/app/app.ts
+++ task-management/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
+
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, FullCalendarModule],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('task-management');
+  
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 }
